8b4b7fe364d2bb0abf8698b322ef2a2fa38e0e76
Mark Gowen <mark.gowen78@gmail.com>
2022-11-01 08:48:57 -0700

    commit tree e057c3f6250b043fa005793d5fc064be16f6ac50

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 // Write your solution here!
+const cats = [
+    "Milo", 
+    "Otis", 
+    "Garfield"
+];
+
+
+function destructivelyAppendCat(name) {
+    return cats.push(name);
+}
+
+function destructivelyPrependCat(name) {
+    return cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat(name) {
+    return cats.pop(name);
+}
+
+function destructivelyRemoveFirstCat(name) {
+    return cats.shift(name);
+}
+
+function appendCat(name) {
+    const moreCats = [...cats, name];
+    return moreCats;
+} 
+
+function prependCat(name) {
+    const evenMoreCats = [name, ...cats];
+    return evenMoreCats;
+}
+
+function removeLastCat() {
+    const lastCats = cats.slice(0, -1);
+    return lastCats;
+}
+
+function removeFirstCat() {
+    const firstCat = cats.slice(1);
+    return firstCat;
+}
